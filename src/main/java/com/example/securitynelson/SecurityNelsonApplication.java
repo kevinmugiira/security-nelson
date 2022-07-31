b553905ee44e3d578c1f2154712fa7ece42433be
Kevin Mugiira <kevinmugiira@gmail.com>
2022-07-31 13:36:45 +0300

    Initial commit

---
+++ src/main/java/com/example/securitynelson/SecurityNelsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitynelson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityNelsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityNelsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/securitynelson/SecurityNelsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitynelson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityNelsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
