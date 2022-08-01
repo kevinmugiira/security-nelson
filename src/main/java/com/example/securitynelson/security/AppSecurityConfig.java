e44c0b54653c75d354deef88fcf76ac055703ab9
Kevin Mugiira <kevinmugiira@gmail.com>
2022-08-01 12:23:17 +0300

    added a student mgt controller, with permissions

--- src/main/java/com/example/securitynelson/security/AppSecurityConfig.java
+++ src/main/java/com/example/securitynelson/security/AppSecurityConfig.java
@@ -4,6 +4,7 @@ package com.example.securitynelson.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.securitynelson.security.ApplicationUserPermission.*;
 import static com.example.securitynelson.security.ApplicationUserRole.*;
 
 @Configuration
@@ -29,9 +31,14 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/","index", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/","index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAnyAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAnyAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAnyAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -45,18 +52,25 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
          UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name()) //ROLE_STUDENT
+                .roles(STUDENT.name()) //implemented the ApplicationUserRole as a static import
                 .build();
 
          UserDetails lindaUser = User.builder()
                  .username("linda")
                  .password(passwordEncoder.encode("password"))
-                 .roles(ADMIN.name())
+                 .roles(ADMIN.name()) //implemented the ApplicationUserRole as a static import
                  .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password"))
+                .roles(ADMINTRAINEE.name()) //implemented the ApplicationUserRole as a static import
+                .build();
 
         return new InMemoryUserDetailsManager(
                 annaSmithUser,
-                lindaUser
+                lindaUser,
+                tomUser
         );
     }
 }
